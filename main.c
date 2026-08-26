4e0a6c173d0998194ffb7b6ca0835da14abfa1f9
AndersonVanzo <anderson.vanzo01@gmail.com>
2026-08-26 15:15:33 -0300

    add base window

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+#include "raylib.h"
 #include <stdio.h>
 
+#define WIDTH 800
+#define HEIGHT 600
+#define TARGET_FPS 30
+
+#define BACKGROUND_COLOR BLACK
+#define JELLYFISH_COLOR RAYWHITE
+
 int main(void) {
-    printf("C Jellyfish\n");
+    InitWindow(WIDTH, HEIGHT, "C Jellyfish");
+    SetTargetFPS(TARGET_FPS);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BACKGROUND_COLOR);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
